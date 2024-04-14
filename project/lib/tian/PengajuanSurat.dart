4d5c02d4830c4872b0c2d793395859b76a69c0b6
Christian-Tiovanto <christiantiovanto1@gmail.com>
2024-04-14 12:22:22 +0700

    feat(PengajuanSurat): menambahkan kolom container untuk kolom kolom yang harus diisi di pengajuan surat

--- project/lib/tian/PengajuanSurat.dart
+++ project/lib/tian/PengajuanSurat.dart
@@ -14,13 +14,45 @@ class _PengajuanSuratState extends State<PengajuanSurat> {
     return MaterialApp(
       home: Scaffold(
         appBar: const _PengajuanSuratAppBarWidget(),
+        body: Column(
+          children: [
+            ContainerKolomPengajuanSurat(),
+          ],
+        ),
       ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
+class ContainerKolomPengajuanSurat extends StatelessWidget {
+  const ContainerKolomPengajuanSurat({
+    super.key,
+  });
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(bottom: 10),
+      decoration:
+          const BoxDecoration(border: Border(bottom: BorderSide(width: 1))),
+      child: SizedBox(
+        height: 40,
+        child: ListTile(
+          horizontalTitleGap: 30,
+          titleAlignment: ListTileTitleAlignment.center,
+          leading: Text(
+            "Dari",
+            style: TextStyle(fontSize: 15, height: 1),
+          ),
+          title: Text('One-line with trailing widget',
+              style: TextStyle(height: 1, fontSize: 15)),
+          trailing: Icon(Icons.keyboard_arrow_down_outlined),
+        ),
+      ),
+    );
+  }
+}
 
 class _PengajuanSuratAppBarWidget extends StatelessWidget
     implements PreferredSizeWidget {
